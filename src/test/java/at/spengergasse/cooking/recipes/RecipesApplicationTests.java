b5400b74c7246714ce62de067d9b8ece339213ea
Tobias Lendl <lendl.tobias@gmail.com>
2023-10-24 08:53:15 +0200

    Initial Spring Setup

---
+++ src/test/java/at/spengergasse/cooking/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package at.spengergasse.cooking.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
