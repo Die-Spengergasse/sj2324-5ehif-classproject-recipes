1d04176c9136209294e467b81336167c865b046e
Tobias Lendl <lendl.tobias@gmail.com>
2023-10-24 08:53:15 +0200

    Initial Spring Setup

---
+++ src/test/java/at/spengergasse/cooking/recipes/TestRecipesApplication.java
@@ -0,0 +1,23 @@
+package at.spengergasse.cooking.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestRecipesApplication {
+
+	@Bean
+	@ServiceConnection
+	MongoDBContainer mongoDbContainer() {
+		return new MongoDBContainer(DockerImageName.parse("mongo:latest"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.from(RecipesApplication::main).with(TestRecipesApplication.class).run(args);
+	}
+
+}
