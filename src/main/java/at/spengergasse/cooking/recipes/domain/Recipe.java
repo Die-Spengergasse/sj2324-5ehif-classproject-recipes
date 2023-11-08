ae3cddeb1415dcd64a42ef3d0821d84ac85e2634
Tobias Lendl <lendl.tobias@gmail.com>
2023-11-08 14:13:39 +0100

    Add hibernate for validation

--- src/main/java/at/spengergasse/cooking/recipes/domain/Recipe.java
+++ src/main/java/at/spengergasse/cooking/recipes/domain/Recipe.java
@@ -1,15 +1,24 @@
 package at.spengergasse.cooking.recipes.domain;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.ZonedDateTime;
+import java.util.List;
 
 @Document("recipes")
 @AllArgsConstructor
 public class Recipe {
     @Id
-    private String key;
+    public String key;
     public String username;
     public String userKey;
+    @Size(min = 1)
+    public List<Ingredient> ingredients;
+    public ZonedDateTime creationTS;
+    public int likes;
 
 }
