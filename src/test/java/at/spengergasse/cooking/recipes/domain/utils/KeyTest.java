11acfeb05db1db7f529ea2b16629d85f129e7a81
Julian <Julian.meyringer@hotmail.com>
2024-01-16 17:41:39 +0100

    Add Key generator Test

--- src/test/java/at/spengergasse/cooking/recipes/domain/utils/KeyTest.java
+++ src/test/java/at/spengergasse/cooking/recipes/domain/utils/KeyTest.java
@@ -17,6 +17,13 @@ public class KeyTest {
     }
 
     @Test
+    public void generateKeys(){
+        for(int i=0; i<10; i++){
+            Key key = KeyType.RECIPE.randomKey();
+            System.out.println(key);
+        }
+    }
+    @Test
     public void parseKey() {
         final var key = KeyType.RECIPE.randomKey().toString();
         assertThat(key).isEqualTo(KeyType.parse(key).toString());
