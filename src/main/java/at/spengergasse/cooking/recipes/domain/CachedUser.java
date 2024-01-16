5aff6a44248d74bb564f021e586ab09328db266e
Dominik Fluch <dominik@diefluchs.at>
2024-01-16 09:34:15 +0100

    Fix convention & Wrong key type

--- src/main/java/at/spengergasse/cooking/recipes/domain/CachedUser.java
+++ src/main/java/at/spengergasse/cooking/recipes/domain/CachedUser.java
@@ -3,17 +3,19 @@ package at.spengergasse.cooking.recipes.domain;
 import at.spengergasse.cooking.recipes.domain.utils.key.Key;
 import at.spengergasse.cooking.recipes.domain.utils.key.KeyType;
 import at.spengergasse.cooking.recipes.service.user.UserDto;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Value;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
+@Getter
+@Setter
+@AllArgsConstructor
 public class CachedUser {
-    public Key userKey;
-    public String username;
-
-    public CachedUser(Key userKey, String username) {
-        this.userKey = userKey.ensureValid(KeyType.USER);
-        this.username = username;
-    }
+    private Key userKey;
+    private String username;
 
     public CachedUser(UserDto userDto) {
         this(KeyType.parse(userDto.key()), userDto.username());
