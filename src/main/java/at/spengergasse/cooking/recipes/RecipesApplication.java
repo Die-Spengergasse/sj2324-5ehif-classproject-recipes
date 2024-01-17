aff527f9f50262bd7dc0e74e4a4ff736a6b5bd20
Julian <Julian.meyringer@hotmail.com>
2024-01-17 16:58:39 +0100

    Delete Wiremock things

--- src/main/java/at/spengergasse/cooking/recipes/RecipesApplication.java
+++ src/main/java/at/spengergasse/cooking/recipes/RecipesApplication.java
@@ -1,6 +1,6 @@
 package at.spengergasse.cooking.recipes;
 
-import org.springframework.boot.SpringApplication;
+ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
