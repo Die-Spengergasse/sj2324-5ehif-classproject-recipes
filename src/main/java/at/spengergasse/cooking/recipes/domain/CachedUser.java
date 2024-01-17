77857dca8159e3e282d1e513a3c5adb76fd5dbc0
FelixKStudent <KUN20418@spengergasse.at>
2024-01-17 08:25:55 +0100

    Fixed cached user constructor missing, get route working again

--- src/main/java/at/spengergasse/cooking/recipes/domain/CachedUser.java
+++ src/main/java/at/spengergasse/cooking/recipes/domain/CachedUser.java
@@ -3,16 +3,14 @@ package at.spengergasse.cooking.recipes.domain;
 import at.spengergasse.cooking.recipes.domain.utils.key.Key;
 import at.spengergasse.cooking.recipes.domain.utils.key.KeyType;
 import at.spengergasse.cooking.recipes.service.user.UserDto;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.Value;
+import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CachedUser {
     private Key userKey;
     private String username;
