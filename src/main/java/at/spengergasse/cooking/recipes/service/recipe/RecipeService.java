ba285abb6142fe770298ff52aec58f676500d3cd
Julian Meyringer <Julian.meyringer@hotmail.com>
2024-04-11 10:44:49 +0200

    STart Fixing Service and DTOs

--- src/main/java/at/spengergasse/cooking/recipes/service/recipe/RecipeService.java
+++ src/main/java/at/spengergasse/cooking/recipes/service/recipe/RecipeService.java
@@ -1,6 +1,11 @@
 package at.spengergasse.cooking.recipes.service.recipe;
 
+import at.spengergasse.cooking.recipes.api.CommentDTO;
+import at.spengergasse.cooking.recipes.api.IngredientDTO;
+import at.spengergasse.cooking.recipes.api.NutrientSummaryDTO;
 import at.spengergasse.cooking.recipes.api.RecipeDTO;
+import at.spengergasse.cooking.recipes.domain.Category;
+import at.spengergasse.cooking.recipes.domain.Difficulty;
 import at.spengergasse.cooking.recipes.domain.Recipe;
 import at.spengergasse.cooking.recipes.domain.CachedUser;
 import at.spengergasse.cooking.recipes.domain.utils.key.Key;
@@ -77,9 +82,18 @@ public class RecipeService {
         List<Recipe> allRecipes = this.recipeRepository.findRecipes();
 
         List<RecipeDTO> RecipesDTOS = allRecipes.stream().map(r -> new RecipeDTO(
-                        r.getTitle()))
+                r.getAuthor(),
+                r.getTitle(),
+                r.getDescription(),
+                r.getIngredients(),
+                r.getCreationTS(),
+                r.getLikes(),
+                r.getNutrientSummary(),
+                r.getCategories(),
+                r.getComments(),
+                r.getDifficulty(),
+                r.getTitlePictureID()))
                 .collect(Collectors.toList());
-
         return RecipesDTOS;
     }
 
